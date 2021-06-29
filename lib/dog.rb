227e8bdfeae5479f210b214ee9a13cdc6bd106ed
suey0128 <syui0128@gmail.com>
2021-06-29 14:59:53 -0700

    commit tree 5eb15f83b07a94193bb4c3da3d51bff26fb04a2e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    #setter method for name 
+    def name=(name)
+        @name = name
+    end
 
+    #getter method for breed
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed= breed
+    end
+
+    def breed 
+        @breed
+    end
 end
